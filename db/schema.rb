0eb0e50f936b946120364856a51d1ccab40c8d22
Lizbeth Loyola <lizbeth@crunchbang.(none)>
2012-12-03 13:30:23 +0800

    make user token authenticatable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121128022434) do
+ActiveRecord::Schema.define(:version => 20121203051649) do
 
   create_table "checkins", :force => true do |t|
     t.integer  "user_id"
@@ -143,6 +143,7 @@ ActiveRecord::Schema.define(:version => 20121128022434) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
+    t.string   "authentication_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
