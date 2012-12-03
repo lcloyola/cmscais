0b3248fce6240f68e623ff51f376d5e954756dc1
Lizbeth Loyola <lizbeth@crunchbang.(none)>
2012-12-03 14:10:06 +0800

    added api for token authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,10 @@
 Cmsschema::Application.routes.draw do
-  
 
-  
-
-  
-
-  
-
-  
-
-  
-
-  
-
-  
-
-  
-
-  
-
-  
-
-  
-
-  
-
-  
-
-  
+  namespace :api do
+    namespace :v1  do
+      resources :tokens,:only => [:create, :destroy]
+    end
+  end
 
   resources :documents
 
